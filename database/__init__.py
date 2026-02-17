77ef8f645ad35875c1c02ec1f93abc3170cf8a45
DeepakThillai <deepakthillai07@gmail.com>
2026-02-18 03:45:47 +0530

    Delete database directory

--- database/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Database package for user context and MongoDB integration
-"""
-
-from .user_context import UserContextManager
-from .mongo_store import MongoStore
-
-__all__ = ["UserContextManager", "MongoStore"]
